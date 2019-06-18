a3134ad381edcf23416e6eec510b020ea0a2e962
Terence Eden <github.com@shkspr.mobi>
2019-06-18 21:17:12 +0100

    Basic skill

---
+++ app.py
@@ -0,0 +1,25 @@
+import os
+from flask import Flask, render_template, request
+from flask_ask import Ask, statement, question, session
+import json, urllib.request
+
+
+app = Flask(__name__)
+ask = Ask(app, '/')
+
+@ask.launch
+def launched():
+    return question('Welcome to Open Benches. What would you like to know?')
+
+@ask.intent("AMAZON.FallbackIntent")
+def fallback():
+    return statement("You can ask for the latest bench, or for a random bench.")
+
+@ask.intent("PrescriptionCosts")
+def prescription_cost():
+    return statement("The prescription charge in England is £9.00 per item.")
+
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
